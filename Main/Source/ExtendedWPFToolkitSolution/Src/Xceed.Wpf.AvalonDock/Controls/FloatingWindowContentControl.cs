697e219d513cfd56691828b10412ea0f5f0fbb20
Michael Pfeiffer <Michael.Pfeiffer@elektrobit.com>
2017-08-21 13:50:11 +0200

    Add FloatingWindowContentControl Use FloatingWindowContentControl in LayoutFloatingWindowControl

---
+++ Main/Source/ExtendedWPFToolkitSolution/Src/Xceed.Wpf.AvalonDock/Controls/FloatingWindowContentControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Xceed.Wpf.AvalonDock.Controls
+{
+    public class FloatingWindowContentControl : ContentControl
+    {
+        static FloatingWindowContentControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FloatingWindowContentControl), new FrameworkPropertyMetadata(typeof(FloatingWindowContentControl)));
+        }
+    }
+}
